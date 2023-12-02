4992da4f4f44eed04a5c2904d569cc34b6b85998
sushil174 <sushilkoregave123@gmail.com>
2023-12-02 08:48:51 -0500

    Fixes

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
+let row = 3;
+let column = 3;
+const button = document.querySelector('button');
+
+//button.addEventListener('click', () => {
+//    let size = prompt("enter the gird");
+//    row,column = parseInt(size);
+//})
+
+
+
 const container = document.querySelector('#container');
 var i,j = 0;
-for(i=0;i<9;i++){
-    for(j=0;j<9;j++){
-        var test = document.createElement('div');
+let GridSize = 960/row;
+for(i=0;i<row;i++){
+    for(j=0;j<column;j++){
+        const test = document.createElement('div');
+        test.classList.add('grid');
         test.style.backgroundColor = "white";
-        test.style.width = "106.66px";
-        test.style.height = "106.66px";
+        test.style.width = `${GridSize}px`;
+        test.style.height = `${GridSize}px`;
         test.style.border = "1px black solid"
         test.style.boxSizing = "border-box";
         container.appendChild(test);
     }
 }
 
-const list = Array.from(document.querySelectorAll('div'));
+const list = Array.from(document.querySelectorAll('div.grid'));
 list.forEach((item) => {
     item.addEventListener('mouseenter', ()=> {
         item.style.backgroundColor = "black";
